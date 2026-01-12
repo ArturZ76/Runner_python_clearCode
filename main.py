7f7dff1815c996691b834f98968023636d4fa7b2
Artur <artur.zajaczkowski76@gmail.com>
2026-01-12 20:04:10 +0100

    snail added

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
+test_font = pygame.font.Font( "font/Pixeltype.ttf", 50 )
 
 sky_surface = pygame.image.load('graphics/Sky.png')
 ground_surface = pygame.image.load('graphics/ground.png')
+text_surface = test_font.render("My game", False, "Black")
+
+snail_surface = pygame.image.load("graphics/snail/snail1.png")
+snail_x_pos = 800
 
 while True:
     for event in pygame.event.get():
@@ -17,6 +22,11 @@ while True:
 
     screen.blit(sky_surface, (0, 0))    #(blit)block image transfer
     screen.blit(ground_surface, (0, 300))
-    
+    screen.blit(text_surface, (350, 50))
+    snail_x_pos -= 2
+    if snail_x_pos <= -70:
+        snail_x_pos = 800
+    screen.blit(snail_surface, (snail_x_pos, 270))
+
     pygame.display.update()
     clock.tick(60) #maxk is 60fps
