6cf9baf7f46f8a0e33d3fd468b5ec547e32f6c7e
Artur <artur.zajaczkowski76@gmail.com>
2026-01-14 19:47:39 +0100

    jumping

--- main.py
+++ main.py
@@ -9,10 +9,17 @@ test_font = pygame.font.Font( "font/Pixeltype.ttf", 50 )
 
 sky_surface = pygame.image.load('graphics/Sky.png').convert() #konwertowanie do formaty optymalniejszego dla Pygame
 ground_surface = pygame.image.load('graphics/ground.png').convert()
-text_surface = test_font.render("My game", False, "Black")
+
+score_surf = test_font.render("My game", False, (64,64,64))
+score_rect = score_surf.get_rect(center = (400, 50))
 
 snail_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
 snail_x_pos = 800
+snail_rect = snail_surface.get_rect(midbottom = (snail_x_pos, 300) )
+
+player_surface = pygame.image.load("graphics/Player/player_walk_1.png").convert_alpha()
+player_rect = player_surface.get_rect(bottomright = ( 80, 300 ))
+player_gravity = 0
 
 while True:
     for event in pygame.event.get():
@@ -20,13 +27,34 @@ while True:
             pygame.quit()
             exit()
 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
+                player_gravity = -20
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+                player_gravity = -20
+
+
     screen.blit(sky_surface, (0, 0))    #(blit)block image transfer
     screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (350, 50))
-    snail_x_pos -= 2
-    if snail_x_pos <= -70:
-        snail_x_pos = 800
-    screen.blit(snail_surface, (snail_x_pos, 270))
+    pygame.draw.rect(screen, '#c0e8ec', score_rect)
+    pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
+    screen.blit(score_surf, score_rect)
+
+    if snail_rect.right <= 0:
+        snail_rect.left = 800
+    screen.blit(snail_surface, snail_rect)
+    snail_rect.right -= 2
+
+    #Player
+    player_gravity += 1
+    player_rect.y += player_gravity
+    if player_rect.bottom >= 300:
+        player_rect.bottom = 300
+    screen.blit(player_surface, player_rect)
+
+ 
 
     pygame.display.update()
     clock.tick(60) #maxk is 60fps
